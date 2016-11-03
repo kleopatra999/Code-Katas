0e37adc7dde683307a7a8ee06ae98d8afa4e19c8
David Roller <david_roller@icloud.com>
2016-11-03 15:28:24 +0100

    Added the copyright

--- FromRomanNumerals/stdafx.h
+++ FromRomanNumerals/stdafx.h
@@ -1,7 +1,24 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
+// Code-Katas
 //
+// Copyright (C) 2016 David Roller
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgement in the product documentation would be
+//    appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
 
 #pragma once
 
@@ -9,7 +26,3 @@
 
 #include <stdio.h>
 #include <tchar.h>
-
-
-
-// TODO: reference additional headers your program requires here
